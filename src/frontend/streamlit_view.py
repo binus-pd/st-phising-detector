0c1fbd3845c01e0afabf2a0a0eadeb84c2d28715
binus-pd <binusphisingdetector@gmail.com>
2024-06-18 23:33:14 +0700

    Update streamlit_view.py

--- src/frontend/streamlit_view.py
+++ src/frontend/streamlit_view.py
@@ -11,21 +11,21 @@ def phising_url_view(data):
   # Form elements
   url = st.text_input("Enter URL:")
   prediction_type = st.selectbox("Prediction Model:", ("All", "RF", "GBC", "ABC"))
-  output = st.text_area("Phishing Prediction:", "", height=100, disabled=True)
 
   # Prediction logic based on selection
   if st.button("Predict"):
     if url:
+      st.text("### Phishing Prediction Result")
       data, Scaler = data_processing(data)
       if prediction_type == "All":
         model_number = 0
         for model_name in ["RF", "GBC", "ABC"]:
           prediction = predict_phishing(url, model_name, data, Scaler)
-          output.text += f"{model_name}: {display_prediction(prediction[model_number])}\n"
+          st.text(f"{model_name}: {display_prediction(prediction[model_number])}\n")
           model_number += 1
       else:
         prediction = predict_phishing(url, prediction_type, data, Scaler)
-        output.text = f"{prediction_type}: {display_prediction(prediction)}\n"
+        st.text(f"{prediction_type}: {display_prediction(prediction)}\n")
         
 def display_prediction(prediction):
     if prediction == 0:
